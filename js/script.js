52d08a68f59ad7932332dff0b88a813b95706bce
kaleb wodimu <99580734+kalebwondimu33@users.noreply.github.com>
2023-09-29 13:47:27 +0300

    Add files via upload
    food ordering stie overview about the company

---
+++ js/script.js
@@ -0,0 +1,82 @@
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+document.addEventListener("DOMContentLoaded", function () {
+  const yearEl = document.querySelector(".year");
+  const currentYear = new Date().getFullYear();
+  yearEl.textContent = currentYear;
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const btnNav = document.querySelector(".btn-mobile-nav");
+  const headerEl = document.querySelector(".header");
+  btnNav.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+  });
+});
+//smooth scrolling /////
+document.addEventListener("DOMContentLoaded", function () {
+  const allLinks = document.querySelectorAll("a");
+  allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const href = link.getAttribute("href");
+      //scroll back to top
+      if (href == "#")
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      if (href !== "#" && href.startsWith("#")) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: "smooth" });
+      }
+      if (link.classList.contains("main-nav-link")) {
+        const headerEl = document.querySelector(".header");
+        headerEl.classList.toggle("nav-open");
+      }
+    });
+  });
+});
+//sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+//fixing flexbox gap property in some safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+/*
+
+*/
